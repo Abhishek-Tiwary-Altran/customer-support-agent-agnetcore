330c4b1889ce8c135576b0b59ae4653d13c91630
Abhishek Kumar Tiwary <71311063+Abhishek-Tiwary-Altran@users.noreply.github.com>
2026-02-11 16:55:20 +0530

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,12 @@
+"""
+AgentCore Complete Example - Utilities Package
+
+This package contains utility functions and helpers for working with
+Amazon Bedrock AgentCore components.
+"""
+
+from .aws_helpers import *
+from .gateway_helpers import *
+
+__version__ = "1.0.0"
+__author__ = "AWS AgentCore Team"
